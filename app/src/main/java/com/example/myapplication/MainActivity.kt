876118cd07ae086e2bb5dd291fe49f0a8d16e4a9
Jlalsyuth <jlal.syuth@gmail.com>
2024-09-06 20:42:25 +0700

    update

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,21 +1,39 @@
 package com.example.myapplication
-
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
 
+        val etNumber1 = findViewById<EditText>(R.id.etNumber1)
+        val etNumber2 = findViewById<EditText>(R.id.etNumber2)
+        val rgOperation = findViewById<RadioGroup>(R.id.rgOperation)
+        val btnCalculate = findViewById<Button>(R.id.btnCalculate)
+
+        btnCalculate.setOnClickListener {
+            val number1 = etNumber1.text.toString().toDouble()
+            val number2 = etNumber2.text.toString().toDouble()
+            val selectedOperation = rgOperation.checkedRadioButtonId
+
+            val result = when (selectedOperation) {
+                R.id.rbAdd -> number1 + number2
+                R.id.rbSubtract -> number1 - number2
+                R.id.rbMultiply -> number1 * number2
+                R.id.rbDivide -> number1 / number2
+                else -> 0.0
+            }
+
+            val intent = Intent(this, ResultActivity::class.java).apply {
+                putExtra("result", result)
+                putExtra("nim", "12345678")
+                putExtra("nama", "Nama Anda")
+            }
+            startActivity(intent)
         }
     }
 }
